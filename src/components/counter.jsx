237a159a29a2c16ec06335f46e6c77e6a23941e6
BridgitKirika <kirikabetty@gmail.com>
2022-05-23 16:31:29 +0300

    added some codes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = { 
         count: 0,   
-        tags : ['tag1', 'tag2', 'tag3'],
+        tags : [],
         imageUrl: "https://picsum.photos/200" 
     };
 
@@ -12,29 +12,38 @@ class Counter extends Component {
         fontWeight : 'bold'
     }
 
+
+    renderTags () {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>
+
+        return <ul>
+        {this.state.tags.map( tag => <li key={ tag}>{ tag }</li>)}
+    </ul>
+    }
     render() { 
         return (
         <div>
-            <img src={this.state.imageUrl} alt="" />
-            <span  style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-
-            <ul>
-                {this.state.tags.map( tag => <li key={ tag}>{ tag }</li>)}
-            </ul>
+            {/* <img src={this.state.imageUrl} alt="" />
+             <span  style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        
+        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        { this.state.tags.length === 0 && 'please create a new tag!'}
+        {this.renderTags ()}
+            
         </div>
         );
     }
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
+    
+    // getBadgeClasses() {
+    //     let classes = "badge m-2 badge-";
+    //     classes += (this.state.count === 0) ? "warning" : "primary";
+    //     return classes;
+    // }
 
-    formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero": count;
-    }
+    // formatCount() {
+    // const { count } = this.state;
+    // return count === 0 ? "Zero": count;
+    // }
 }
 
 export default Counter;
