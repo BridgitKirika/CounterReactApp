514f99c393b42fedb39d45df2526b9662aeb832b
BridgitKirika <kirikabetty@gmail.com>
2022-05-23 16:18:25 +0300

    added an array to the counter function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = { 
         count: 0,   
+        tags : ['tag1', 'tag2', 'tag3'],
         imageUrl: "https://picsum.photos/200" 
     };
 
@@ -17,6 +18,10 @@ class Counter extends Component {
             <img src={this.state.imageUrl} alt="" />
             <span  style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
+
+            <ul>
+                {this.state.tags.map( tag => <li key={ tag}>{ tag }</li>)}
+            </ul>
         </div>
         );
     }
