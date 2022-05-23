bc7d02305648cf49d40a17a2325d48bc6ed4f25a
BridgitKirika <kirikabetty@gmail.com>
2022-05-23 15:51:01 +0300

    added some styles to the code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0,    
+        count: 0,   
+        imageUrl: "https://picsum.photos/200" 
     };
+
+    styles ={
+        fontSize : 10,
+        fontWeight : 'bold'
+    }
 
     render() { 
         // React.createElement('div')
         return (
         <div>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <img src={this.state.imageUrl} alt="" />
+            <span  style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
     }
@@ -18,7 +25,7 @@ class Counter extends Component {
 
     formatCount() {
     const { count } = this.state;
-    return count === 0 ? "zero": count;
+    return count === 0 ? "Zero": count;
     }
 }
 
